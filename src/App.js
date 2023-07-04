285417195c969381178d1e8a22dde6ba37a4079e
Foued Krichene <foued@krichene.com>
2023-07-04 21:23:16 +0200

    changed app text and updated tests

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save then Reload.
+          Edit <code>src/App.js</code> and saaave then Reload.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          I was changed in the feature branch.
         </a>
       </header>
     </div>
